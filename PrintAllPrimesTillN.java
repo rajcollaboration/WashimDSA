85f65b64100b2bba0630f29fedaddeeafe7f85b1
rajcollabotation <washimrejakhan@gmail.com>
2023-12-14 00:26:18 +0530

    update prime number solution

--- PrintAllPrimesTillN.java
+++ PrintAllPrimesTillN.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class PrintAllPrimesTillN {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int min_Num = scn.nextInt();
-        int max_Num = scn.nextInt();
-        for (int i = min_Num; i < max_Num; i++) {
-            int div = 2;
-            while (div <= i) {
-                if (i % div == 0) {
-                    div ++;
+        int n = scn.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Please Enter A number");
+            int t = scn.nextInt();
+            int count = 0;
+            for (int div = 2; div * div <= t; div++) {
+                if (t % div == 0) {
+                    count++;
                     break;
                 }
 
-               div++;
-
             }
-             if ( div < min_Num) {
-                    System.out.println("prime");
-                }else{
-                    System.out.println("Not prime");
-                }
+            if (count > 0) {
+                System.out.println("This number is not prime");
+            } else {
+                System.out.println("This number is prime");
+            }
+
         }
     }
 }
